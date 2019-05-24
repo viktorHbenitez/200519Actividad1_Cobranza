09149aee36f85b1afda8b7d3880455153e463dd6
Victor Hugo Benitez Bosques <victorHbenitez85@hotmail.com>
2019-05-23 23:10:52 -0500

    Add gif activity 5

---
+++ 200519Actividad1_Registro/CobranzaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CobranzaViewController.swift
+//  200519Actividad1_Registro
+//
+//  Created by Victor Hugo Benitez Bosques on 23/05/19.
+//  Copyright © 2019 Victor Hugo Benitez Bosques. All rights reserved.
+//
+
+import UIKit
+
+class CobranzaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
